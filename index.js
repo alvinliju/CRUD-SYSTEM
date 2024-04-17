244b27e212e761b796e0c1e890121d66b75549f3
Alvin Liju <89293506+alvinliju@users.noreply.github.com>
2024-04-17 17:29:11 +0530

    defined routes and controllers

--- index.js
+++ index.js
@@ -2,47 +2,31 @@ const express = require('express')
 const app = express()
 const path = require('path');
 const mongoose = require("mongoose");
+const customerRoute=require('./routes/customer.route')
 const Customer = require('./models/customer.models')
+const { getCustomer,
+    createCustomer,
+    findCustomer
+} = require('./controllers/customer.controller')
 
+
+//middlewares
 app.use(express.json());
 
+// routes
 app.get('/', (req, res) => {
   res.send('Hello World!')
 });
 
-app.get('/customer',(req, res)=>{
-    res.sendFile(path.join(__dirname+'/frontend/customer.html'));
-});
+app.use('/customer',customerRoute);
 
-app.post('/customer/custom',(req,res)=>{
-    res.send('ninte achan thane');
-    const name = req.body.fname;
-    console.log(`Submitted Name: ${name}`);
-});
+app.post('/customer', createCustomer);
 
-app.post('/customer', async(req,res)=>{
-    try {
-        const customer = await Customer.create(req.body);
-        res.status(200).json(customer);
-      } catch (error) {
-        res.status(500).json({ message: error.message });
-      }
-})
+app.get('/customer/:tableNumber', getCustomer)
 
-app.get('/customer/:tableNumber',async (req,res)=>{
-    try{
-        const {tableNumber} = req.params;
-        const customer = await Customer.findOne({ table: tableNumber });
-        console.log('kutti mama kitii')
-        if(!customer){
-            return res.status(404).json({ message: "Customer not found" });
-        }
-        res.status(200).json(customer);
-    } catch(error){
-        res.status(500).json({ message: error.message });
-    }
-})
 
+
+//db connection
 mongoose.connect("mongodb+srv://admin:parayulla@cluster0.ymgspgk.mongodb.net/fooddb?retryWrites=true&w=majority")
   .then(() => {
     console.log("connected to database");
